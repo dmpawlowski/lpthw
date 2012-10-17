5004a7d38aed321323c94f1f81c0a84fccb83640
Don P <don.m.pawlowski@gmail.com>
2012-10-17 18:45:43 -0400

    Completed some week 3 exercises

---
+++ ex33.py
@@ -0,0 +1,17 @@
+def while_loop(end, increment):
+    i = 0 
+    numbers = []
+
+    while i < end:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        
+        i = i + increment
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+        
+        
+    print "The numbers: "
+
+    for num in numbers:
+        print num
---
+++ ex33b.py
@@ -0,0 +1,9 @@
+def for_loop(end):
+    i = [1, 2, 3, 4, 5, 6]
+
+    for number in i:
+        print "At the top i is %d" % number        
+        
+    print "The numbers: "
+
+    for num in numbers:
