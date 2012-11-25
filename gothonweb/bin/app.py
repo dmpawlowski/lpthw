66c90b20c21bf591ca83e7bf4f6827f8b9a51958
Don P <don.m.pawlowski@gmail.com>
2012-11-25 12:58:02 -0500

    added gothon web form

--- gothonweb/bin/app.py
+++ gothonweb/bin/app.py
@@ -10,14 +10,12 @@ render = web.template.render('templates/')
 
 class Index(object):
     def GET(self):
-        form = web.input(name="Nobody", greet=None)
-          if form.greet:
-            greeting = %s, %s % (form.greet, form.name)
-            return render.index(greeting = greeting)
-          else:
-            return "ERROR: greet is required"
-        greeting = "%s %s" % (form.greet, form.name)
-        return render.index(greeting = greeting)
+      return render.hello_form()
+
+    def POST(self):
+      form = web.input(name="Nobody", greet="Hello")
+      greeting = "%s, %s" % (form.greet, form.name)
+      return render.index(greeting = greeting)
 
 if __name__ == "__main__":
     app.run()
